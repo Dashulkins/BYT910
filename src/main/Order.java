dead0a07e9b8dc9884f3fb95f5ec1f307181822a
Danylo Hrebeniuk <dhr@touk.pl>
2022-12-14 20:30:11 +0100

    new class Order

---
+++ src/main/Order.java
@@ -0,0 +1,36 @@
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Order {
+    private Long id;
+    private LocalDateTime dateOfPlacement;
+    private String address;
+    private LocalDateTime estimatedDeliveryDate;
+    private Status status;
+    private Customer customer;
+    private List<Shoes> itemsWithPrice;
+    private boolean isPaid;
+    public enum Status {
+        DELIVERED, READY_TO_BE_SHIPPED, SHIPPING, ASSEMBLED, CANCELED
+    }
+
+    public void payForOrder() {
+        this.isPaid = true;
+    }
+
+    public void placeOrder() {
+        dateOfPlacement = LocalDateTime.now();
+    }
+
+    public void provideDeliveryDetails(String address) {
+        this.address = address;
+    }
+
+    public void cancelOrder() {
+        status = Status.CANCELED;
+    }
+
+    public Status checkCustomerOrder() {
+        return status;
+    }
+}
